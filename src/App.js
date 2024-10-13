bbe4d32d99810f57d849439ef43fd00402d20146
nanaring1 <adelya1234@naver.com>
2024-10-13 23:53:12 +0900

    라우터 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Children } from "react";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+// import Mainpage from "./pages/MainPage";
+
+const App = () => {
+  const routes = useRoutes([
+    { path: '/', element: <MainPage /> },
+    {
+      path: '/picture', element: <PictureMain />,
+      children: [
+        { path: '', element: <PictureResult /> }
+      ]
+    },
+    {
+      path: '/edit-picture', element: <EditPicture />,
+      children: [
+        { path: '', element: <SelectFrame /> }
+      ]
+    },
+    {
+      path: '/success', element: <Success />,
+      children: [
+        { path: '', element: <SuccessResult /> }
+      ]
+    }
+  ]);
+
+  return routes;
+};
+
+const AppWrapper = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
