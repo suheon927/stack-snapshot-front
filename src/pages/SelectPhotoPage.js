10da4de6827ebdf8f9d83e5bf1da2946c67dc624
kimehyun <lh7721004@naver.com>
2024-11-03 00:28:04 +0900

    [UPDATE] 선택된 이미지 강조, toggle, queryparameter로 자동으로 이미지를 불러옴

--- src/pages/SelectPhotoPage.js
+++ src/pages/SelectPhotoPage.js
@@ -14,20 +14,18 @@ import blank from "../images/blank.png"
  * @author 김이현
  */
 const SelectPhotoPage = () => {
+
+
+  
     const FRAMES = {
         "frame1":{"shape_small":[125,192],"shape_big":[330,506],"realsize":[600,920],"frame":frame1,"selected":false,"frameID":1,"maxCount":4},
         "frame2":{"shape_small":[125,192],"shape_big":[330,506],"realsize":[600,920],"frame":frame2,"selected":false,"frameID":2,"maxCount":6},
         "frame3":{"shape_small":[125,192],"shape_big":[330,506],"realsize":[600,920],"frame":frame3,"selected":false,"frameID":3,"maxCount":4},
         "frame4":{"shape_small":[188,125],"shape_big":[495,330],"realsize":[900,600],"frame":frame4,"selected":false,"frameID":4,"maxCount":4}
     };
-    let images = ["https://image.utoimage.com/preview/cp872722/2022/12/202212008462_500.jpg",
-    "https://helpx.adobe.com/content/dam/help/en/photoshop/using/quick-actions/remove-background-before-qa1.png",
-    "https://png.pngtree.com/thumb_back/fh260/background/20230609/pngtree-three-puppies-with-their-mouths-open-are-posing-for-a-photo-image_2902292.jpg",
-    "https://cdn.pixabay.com/photo/2024/02/26/19/39/monochrome-image-8598798_1280.jpg",
-    "https://cdn.aitimes.kr/news/photo/202303/27617_41603_044.jpg",
-  "https://images.unsplash.com/photo-1529927066849-79b791a69825?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D"]
-    //초기에 불러오는 사진들
-    const [photos, setPhotos] = useState(images);
+
+  //초기에 불러오는 사진들
+    const [photos, setPhotos] = useState([]);
 
     //선택한 사진들, 순서대로
     const [SelectedPhotos, setSelectedPhotos] = useState([blank,blank,blank,blank,blank,blank]);
@@ -35,12 +33,26 @@ const SelectPhotoPage = () => {
     const [searchParams, setSearchParams]=useSearchParams();
     const [index, setIndex]=useState(0);
     const navigate = useNavigate();
+
+    // PictureCompletedPage에서 넘겨주는 쿼리 파라미터 3개 frameid groupid date
     const frameid = searchParams.get('frameid');
-    const groupid = searchParams.get("GroupID");
+    const groupid = searchParams.get("groupid");
+    const date = searchParams.get("date");
 
     useEffect(() => {
+      // PictureCompletedPage에서 넘겨주는 쿼리파라미터 frameid값으로 frame 선택
       setCurrnetFrame(FRAMES[`frame${frameid}`])
-    }, [frameid,groupid]);//frameid,groupid에 종속적인
+
+      // PictureCompletedPage에서 넘겨주는 쿼리파라미터 date, groupid값으로 file api 이용해서 이미지 불러오기
+      setPhotos([
+      `http://localhost:8080/api/file?date=`+date+`&type=original&groupid=`+groupid+`&index=0`,
+      `http://localhost:8080/api/file?date=`+date+`&type=original&groupid=`+groupid+`&index=1`,
+      `http://localhost:8080/api/file?date=`+date+`&type=original&groupid=`+groupid+`&index=2`,
+      `http://localhost:8080/api/file?date=`+date+`&type=original&groupid=`+groupid+`&index=3`,
+      `http://localhost:8080/api/file?date=`+date+`&type=original&groupid=`+groupid+`&index=4`,
+      `http://localhost:8080/api/file?date=`+date+`&type=original&groupid=`+groupid+`&index=5`,
+    ]);
+    }, [date,groupid,frameid]);//frameid,groupid에 종속적인 useEffect(), frameid,groupid를 받아와야 실행함
     const checkFrames = ()=>{
       for(let i=0;i<currnetFrame["maxCount"];i++){
         if(SelectedPhotos[i]==blank){
@@ -123,7 +135,8 @@ const SelectPhotoPage = () => {
         });
     
         if (response.ok) {
-          console.log(await response.json());
+          let res = await response.json();
+          await navigate(`/picture/success?groupid=${groupid}&date=${res['outputPath']}`);
         } else {
           console.error("Failed to send files");
         }
@@ -131,6 +144,14 @@ const SelectPhotoPage = () => {
         console.error("Error sending files:", error);
       }
     };
+  const togglePhoto =(index)=>{
+    if(SelectedPhotos.indexOf(photos[index])!=-1){
+      removePhoto(SelectedPhotos.indexOf(photos[index]));
+    }
+    else{
+      addPhoto(index);
+    }
+  }
   return (
     <div className="big-grid">
         <div><img className="logo" src={logo} alt="logo image"></img></div>
@@ -162,7 +183,7 @@ const SelectPhotoPage = () => {
           <div className="photo-grid">
           {photos.map((value, index) => {
               return ( 
-                <div onClick={()=>{addPhoto(index)}} className={`${index}`} key={index}><img src={value}/></div>
+                <img key={index} onClick={()=>{togglePhoto(index)}} className={`photo ${index} ${SelectedPhotos.includes(value) ? "selected-photo" : ""}`} src={value}  onError={(err)=>{console.log(170,err);}}/>
                 );
               })}
           </div>
@@ -174,18 +195,18 @@ const SelectPhotoPage = () => {
         <div></div>
         <div className="next-btn">{checkFrames()
           ?
-            <Link onClick={sendFiles}>
+            // <Link onClick={sendFiles}>
                 <svg className="send-file-btn" onClick={sendFiles} width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <rect width="75" height="75" rx="12" fill="#080E1C"/>
                     <path d="M35.1328 47.3594L43.5703 38.9219H23.7266V34.3125H43.5703L35.1328 25.9141L38.2969 22.7891L52.1641 36.6172L38.2969 50.4844L35.1328 47.3594Z" fill="#D2D5DC"/>
                 </svg>
-            </Link>
+            // </Link>
             :
             <></>}</div>
         {<>
           {[...Array(currnetFrame["maxCount"]).keys()].map((value, index) => {
               return (
-                <img className={`frame frame${currnetFrame["frameID"]} picture picture${index+1}`} src={SelectedPhotos[value]} alt="logo image" onClick={()=>{removePhoto(value)}}></img>
+                <img key={index} className={`frame frame${currnetFrame["frameID"]} picture picture${index+1}`} src={SelectedPhotos[value]} alt="logo image" onClick={()=>{removePhoto(index)}}></img>
                 );
             })}
         </>}
